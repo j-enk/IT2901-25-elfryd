a45b63c602283eb720cdd50b6518b674ea1f2521
Sigve Kulsrud <sigvefk@ntnu.no>
2025-04-02 13:27:20 +0200

    Code for displaying temperature from a bme280 sensor

---
+++ temperature-sensor/bme280-temp/src/main.c
@@ -0,0 +1,40 @@
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/sensor.h>
+#include <zephyr/devicetree.h>
+
+#define SLEEP_TIME_MS 1000
+
+const struct device *bme280_dev = DEVICE_DT_GET_ANY(bosch_bme280);
+static struct sensor_value temperature;
+
+void main(void) {
+
+    int err;
+
+    if (!device_is_ready(bme280_dev)) {
+        printk("Device is not ready\n");
+        return;
+    }
+    while(true) {
+
+        do {
+            err = sensor_sample_fetch(bme280_dev);
+            if (err < 0) {
+                printk("Failed to fetch sensor data: %d", err);
+                break;
+            }
+			
+            err = sensor_channel_get(bme280_dev, SENSOR_CHAN_AMBIENT_TEMP, &temperature);
+            if (err < 0) {
+                printk("Failed to get temperature: %d", err);
+                break;
+            }
+
+            printk("Temperature: %.2f Celsius ", sensor_value_to_double(&temperature));
+
+        } while (false);
+
+        k_msleep(SLEEP_TIME_MS);
+    }
+}
