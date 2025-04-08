d2b1405b4de5f3a505e14f2cd52d8ba28cdcc263
Magnus Hansen Åsheim <magnhaas@stud.ntnu.no>
2025-04-08 09:55:18 +0200

    initialize .NET

---
+++ battery-sensor/server/BatterySensorAPI/Program.cs
@@ -0,0 +1,39 @@
+// Program.cs
+using BatterySensorAPI.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Register HttpClient for BatterySensorService
+builder.Services.AddHttpClient<BatterySensorService>();
+builder.Services.AddScoped<BatterySensorService>();
+
+// Add CORS support
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", 
+        builder => builder
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseCors("AllowAll");
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
