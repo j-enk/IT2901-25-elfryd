1964488ecf56d39bcfd08a723ad4caaa22505e71
Magnus Hansen Åsheim <magnhaas@stud.ntnu.no>
2025-04-09 13:36:46 +0200

    remove code from previous group

--- battery-sensor/server/frontend/features/user/userSelector.ts
+++
@@ -1,4 +0,0 @@
-import { RootState } from "../../services/store";
-export const selectActiveUser = (state: RootState) => state.user.activeUser;
-export const selectUserStatus = (state: RootState) => state.user.status;
-export const selectUserError = (state: RootState) => state.user.error;
--- battery-sensor/server/frontend/typings/keyEvent.ts
+++
@@ -1,6 +0,0 @@
-export type KeyEventsType = {
-  action: number;
-  lockBoxId: number;
-  timeStamp: string;
-  userName: string;
-};
