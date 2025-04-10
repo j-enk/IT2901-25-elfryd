b1d3246c39fd2497a8fd478300a27626df6c0ed1
martivj <martinvagseterjakobsen@yahoo.no>
2025-04-10 17:42:32 +0200

    fix seed script with new table formats

--- battery-sensor/broker/seed.sh
+++ battery-sensor/broker/seed.sh
@@ -79,62 +79,58 @@ echo "Generating battery data..."
 base_timestamp=$(get_timestamp)
 
 # Generate battery data for multiple battery IDs
-for battery_id in 11 12 13 14 15; do
+for battery_id in 1 2 3 4; do
     # Generate multiple readings per battery with slightly different timestamps and voltages
-    for i in {1..3}; do
+    for i in {1..4}; do
         ts=$((base_timestamp - i*300))
-        voltage=$((15000 + RANDOM % 2000))
+        voltage=$((7000 + RANDOM % 6000))
         send_battery_reading "$battery_id" "$voltage" "$ts"
     done
 done
 
 echo "Generating temperature data..."
-# Generate temperature data for multiple sensors
-for sensor_id in 1 2 3 4; do
-    # Generate multiple readings per sensor
-    for i in {1..2}; do
-        ts=$((base_timestamp - i*600))
-        temperature=$((20 + RANDOM % 15))
-        send_temperature_reading "$sensor_id" "$temperature" "$ts"
-    done
+# Generate temperature data for one sensor
+for i in {1..12}; do
+    ts=$((base_timestamp - i*600))
+    temperature=$((20 + RANDOM % 15))
+    send_temperature_reading "$temperature" "$ts"
 done
 
+
 echo "Generating gyroscope data..."
-# Generate gyro data for multiple sensors
-for sensor_id in 1 2 3; do
-    # Generate readings with realistic accelerometer and gyroscope values
-    ts=$((base_timestamp - RANDOM % 500))
-    accel_x=$((RANDOM % 10000000 - 5000000))
-    accel_y=$((RANDOM % 10000000 - 5000000))
-    accel_z=$((RANDOM % 10000000 - 5000000))
-    gyro_x=$((RANDOM % 500000 - 250000))
-    gyro_y=$((RANDOM % 500000 - 250000))
-    gyro_z=$((RANDOM % 500000 - 250000))
-    
-    # Format gyro values to include leading zeros
-    formatted_gyro_x=$(printf "%07d" ${gyro_x#-})
-    if [[ $gyro_x -lt 0 ]]; then
-        formatted_gyro_x="-$formatted_gyro_x"
-    else
-        formatted_gyro_x="0$formatted_gyro_x"
-    fi
-    
-    formatted_gyro_y=$(printf "%07d" ${gyro_y#-})
-    if [[ $gyro_y -lt 0 ]]; then
-        formatted_gyro_y="-$formatted_gyro_y"
-    else
-        formatted_gyro_y="0$formatted_gyro_y"
-    fi
-    
-    formatted_gyro_z=$(printf "%07d" ${gyro_z#-})
-    if [[ $gyro_z -lt 0 ]]; then
-        formatted_gyro_z="-$formatted_gyro_z"
-    else
-        formatted_gyro_z="0$formatted_gyro_z"
-    fi
-    
-    send_gyro_reading "$sensor_id" "$accel_x" "$accel_y" "$accel_z" "$formatted_gyro_x" "$formatted_gyro_y" "$formatted_gyro_z" "$ts"
-done
+# Generate readings with realistic accelerometer and gyroscope values
+ts=$((base_timestamp - RANDOM % 500))
+accel_x=$((RANDOM % 10000000 - 5000000))
+accel_y=$((RANDOM % 10000000 - 5000000))
+accel_z=$((RANDOM % 10000000 - 5000000))
+gyro_x=$((RANDOM % 500000 - 250000))
+gyro_y=$((RANDOM % 500000 - 250000))
+gyro_z=$((RANDOM % 500000 - 250000))
+
+# Format gyro values to include leading zeros
+formatted_gyro_x=$(printf "%07d" ${gyro_x#-})
+if [[ $gyro_x -lt 0 ]]; then
+    formatted_gyro_x="-$formatted_gyro_x"
+else
+    formatted_gyro_x="$formatted_gyro_x"
+fi
+
+formatted_gyro_y=$(printf "%07d" ${gyro_y#-})
+if [[ $gyro_y -lt 0 ]]; then
+    formatted_gyro_y="-$formatted_gyro_y"
+else
+    formatted_gyro_y="$formatted_gyro_y"
+fi
+
+formatted_gyro_z=$(printf "%07d" ${gyro_z#-})
+if [[ $gyro_z -lt 0 ]]; then
+    formatted_gyro_z="-$formatted_gyro_z"
+else
+    formatted_gyro_z="$formatted_gyro_z"
+fi
+
+send_gyro_reading "$accel_x" "$accel_y" "$accel_z" "$formatted_gyro_x" "$formatted_gyro_y" "$formatted_gyro_z" "$ts"
+
 
 echo "Sending configuration commands..."
 # Send various configuration commands
@@ -147,16 +143,6 @@ send_config_command "battery"
 send_config_command "temp"
 send_config_command "battery"
 send_config_command "gyro"
-
-echo "Sending test verification messages..."
-# Send some generic test messages
-for i in {1..3}; do
-    message="Test message $i at $(date)"
-    topic="test/verification"
-    echo "Sending test message: $message"
-    mosquitto_pub -h "$HOST" -p "$PORT" --cafile "$CA_FILE" -t "$topic" -m "$message"
-    sleep 0.5
-done
 
 echo ""
 echo "All test data has been sent successfully!"
