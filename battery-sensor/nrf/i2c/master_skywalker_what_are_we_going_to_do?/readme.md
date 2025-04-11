e8c6914436cfe649c2b009dc496619694cc93776
David Spilde <david.s.spilde@gmail.com>
2025-04-11 11:36:33 +0200

    i2c funker

---
+++ battery-sensor/nrf/i2c/master_skywalker_what_are_we_going_to_do?/readme.md
@@ -0,0 +1,2 @@
+nrfjprog --program build/zephyr/zephyr.hex --chiperase --reset -f nrf51 --snr 681130685 --verify
+
--- battery-sensor/nrf/i2c/master_skywalker_what_are_we_going_to_do?/src/main.c
+++ battery-sensor/nrf/i2c/master_skywalker_what_are_we_going_to_do?/src/main.c
@@ -1,16 +1,42 @@
-/*
- * Copyright (c) 2019 Nordic Semiconductor ASA
- *
- * SPDX-License-Identifier: Apache-2.0
- */
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/i2c.h>
+#include <zephyr/logging/log.h>
 
- #include <zephyr/kernel.h>
+LOG_MODULE_REGISTER(i2c_master, LOG_LEVEL_INF);
 
- int main(void)
- {
- #ifdef CONFIG_SAMPLE_DO_OUTPUT
-     printk("Hello World from minimal!\n");
- #endif
- 
-     return 0;
- }
+#define I2C_ADDR 0x69  // Address of the TinyGo target device
+
+int main(void)
+{
+
+    uint8_t buf[16];
+    int num_bytes = 4;
+    int ret;
+
+    // shell get device
+    const struct device *dev = device_get_binding("i2c@40003000");
+
+	if (IS_ENABLED(CONFIG_DEVICE_DT_METADATA) && dev == NULL) {
+		dev = device_get_by_dt_nodelabel("i2c@40003000");
+	}
+    if(!dev){
+        LOG_ERR("I2C device not found");
+        return 0; 
+    }
+
+    while(1){
+        k_msleep(1000);
+    ret = i2c_read(dev, buf, num_bytes, 0x69);
+    if (ret) {
+        LOG_ERR("I2C read failed: %d", ret);
+    }
+    else{
+        printk("I2C read successful: %02x %02x %02x %02x", 
+            (unsigned int)buf[0], (unsigned int)buf[1], 
+            (unsigned int)buf[2], (unsigned int)buf[3]);
+    }
+    }
+
+    return 0;
+}
