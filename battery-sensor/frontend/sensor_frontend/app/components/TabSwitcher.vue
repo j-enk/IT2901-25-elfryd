331c5ae7b0813117a87df5955fe9e3dcc2ec3d41
leifeggenfellner <eggenfellner@protonmail.com>
2025-04-30 15:02:39 +0200

    refactor: emit `ElfrydDashboard.vue`, moved to `index.vue`

--- battery-sensor/frontend/sensor_frontend/app/components/TabSwitcher.vue
+++ battery-sensor/frontend/sensor_frontend/app/components/TabSwitcher.vue
@@ -1,55 +1,76 @@
 <script setup lang="ts">
-import type { BatteryData, ConfigEntry, ParsedCommandResult } from '~/types/elfryd'
-import { Tab } from '~/types/enums/Tab'
+import { Tab } from "~/types/enums/Tab";
+import type { DefineComponent } from "vue";
 
 const props = defineProps<{
-    BatterySection: any
-    GyroSection: any
-    TempSection: any
-    ConfigSection: any
-
-    activeTab: Tab
-}>()
+  activeTab: Tab;
+  batterySection: DefineComponent;
+  gyroSection: DefineComponent;
+  tempSection: DefineComponent;
+  configSection: DefineComponent;
+}>();
 
 const emit = defineEmits<{
-    (e: 'update:activeTab', value: Tab): void
-}>()
+  (e: "update:activeTab", value: Tab): void;
+}>();
 
 const changeTab = (tab: Tab) => {
-    emit('update:activeTab', tab)
-}
+  emit("update:activeTab", tab);
+};
 </script>
 
 <template>
-    <div class="tabs tabs-bordered w-full">
-
-        <!-- Battery -->
-        <input type="radio" name="elfryd_tabs" class="tab" aria-label="Battery"
-            :checked="props.activeTab === Tab.Battery" @change="changeTab(Tab.Battery)" />
-        <div v-if="props.activeTab === Tab.Battery" class="tab-content bg-base-100">
-            <component :is="BatterySection" />
-        </div>
-
-        <!-- Gyro -->
-        <input type="radio" name="elfryd_tabs" class="tab" aria-label="Gyro" :checked="props.activeTab === Tab.Gyro"
-            @change="changeTab(Tab.Gyro)" />
-        <div v-if="props.activeTab === Tab.Gyro" class="tab-content bg-base-100">
-            <component :is="GyroSection" />
-        </div>
-
-        <!-- Temp -->
-        <input type="radio" name="elfryd_tabs" class="tab" aria-label="Temp" :checked="props.activeTab === Tab.Temp"
-            @change="changeTab(Tab.Temp)" />
-        <div v-if="props.activeTab === Tab.Temp" class="tab-content bg-base-100">
-            <component :is="TempSection" />
-        </div>
-
-        <!-- Config -->
-        <input type="radio" name="elfryd_tabs" class="tab" aria-label="Config" :checked="props.activeTab === Tab.Config"
-            @change="changeTab(Tab.Config)" />
-        <div v-if="props.activeTab === Tab.Config" class="tab-content bg-base-100">
-            <component :is="ConfigSection" />
-        </div>
-
+  <div class="tabs tabs-bordered w-full">
+    <!-- Battery -->
+    <input
+      type="radio"
+      name="elfryd_tabs"
+      class="tab"
+      aria-label="Battery"
+      :checked="props.activeTab === Tab.Battery"
+      @change="changeTab(Tab.Battery)"
+    />
+    <div v-if="props.activeTab === Tab.Battery" class="tab-content bg-base-100">
+      <component :is="batterySection" />
     </div>
+
+    <!-- Gyro -->
+    <input
+      type="radio"
+      name="elfryd_tabs"
+      class="tab"
+      aria-label="Gyro"
+      :checked="props.activeTab === Tab.Gyro"
+      @change="changeTab(Tab.Gyro)"
+    />
+    <div v-if="props.activeTab === Tab.Gyro" class="tab-content bg-base-100">
+      <component :is="gyroSection" />
+    </div>
+
+    <!-- Temp -->
+    <input
+      type="radio"
+      name="elfryd_tabs"
+      class="tab"
+      aria-label="Temp"
+      :checked="props.activeTab === Tab.Temp"
+      @change="changeTab(Tab.Temp)"
+    />
+    <div v-if="props.activeTab === Tab.Temp" class="tab-content bg-base-100">
+      <component :is="tempSection" />
+    </div>
+
+    <!-- Config -->
+    <input
+      type="radio"
+      name="elfryd_tabs"
+      class="tab"
+      aria-label="Config"
+      :checked="props.activeTab === Tab.Config"
+      @change="changeTab(Tab.Config)"
+    />
+    <div v-if="props.activeTab === Tab.Config" class="tab-content bg-base-100">
+      <component :is="configSection" />
+    </div>
+  </div>
 </template>
