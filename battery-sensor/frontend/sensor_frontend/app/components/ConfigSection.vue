95f13d1fc9e53f03ac1bbeaa53997dd0fa048928
leifeggenfellner <eggenfellner@protonmail.com>
2025-04-30 15:01:32 +0200

    chore: update code formattning

--- battery-sensor/frontend/sensor_frontend/app/components/ConfigSection.vue
+++ battery-sensor/frontend/sensor_frontend/app/components/ConfigSection.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import type { ConfigEntry, ParsedCommandResult } from '~/types/elfryd'
-import ConfigForm from '~/components/forms/ConfigForm.vue'
-import ConfigDataDisplay from '~/components/displays/ConfigDataDisplay.vue'
+import ConfigForm from "~/components/forms/ConfigForm.vue";
+import ConfigDataDisplay from "~/components/displays/ConfigDataDisplay.vue";
 </script>
 
 <template>
-    <section class="w-full max-w-5xl mx-auto rounded-lg mt-4 p-4 bg-base-300 border-1 border-success">
-        <div class="flex flex-col gap-6">
-            <ConfigForm />
-            <ConfigDataDisplay />
-        </div>
-    </section>
+  <section
+    class="w-full max-w-5xl mx-auto rounded-lg mt-4 p-4 bg-base-300 border-1 border-success"
+  >
+    <div class="flex flex-col gap-6">
+      <ConfigForm />
+      <ConfigDataDisplay />
+    </div>
+  </section>
 </template>
--- battery-sensor/frontend/sensor_frontend/app/components/GyroSection.vue
+++ battery-sensor/frontend/sensor_frontend/app/components/GyroSection.vue
@@ -1,9 +1,10 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
-    <section class="w-full max-w-5xl mx-auto rounded-lg p-6 mt-4" aria-labelledby="data-section-title">
-        <h1 id="data-section-title" class="text-2xl font-bold mb-4">Gyro</h1>
-    </section>
+  <section
+    class="w-full max-w-5xl mx-auto rounded-lg p-6 mt-4"
+    aria-labelledby="data-section-title"
+  >
+    <h1 id="data-section-title" class="text-2xl font-bold mb-4">Gyro</h1>
+  </section>
 </template>
